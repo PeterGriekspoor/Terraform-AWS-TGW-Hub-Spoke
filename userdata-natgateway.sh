ab53daf5760a422baf4bce0b64f1a7dca6ec97d6
Peter <40786627+PeterGriekspoor@users.noreply.github.com>
2021-08-31 16:39:13 +0300

    Update userdata-natgateway.sh

--- userdata-natgateway.sh
+++ userdata-natgateway.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 sudo apt-get install net-tools -y
 sudo apt-get update -y
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-sudo apt-get -y install iptables-persistent
 sudo bash -c 'echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf'
-sudo bash -c 'iptables-save > /etc/iptables/rules.v4'
 sudo ifconfig eth1 10.7.1.10/24 up
 sudo route add -net 10.0.0.0/8 gw 10.7.1.1
 sudo sysctl -w net.ipv4.ip_forward=1
@@ -21,6 +17,12 @@ sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8081 -j DNAT --to 10.1
 sudo iptables -A FORWARD -p tcp -d 10.10.1.20 --dport 80 -j ACCEPT
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8082 -j DNAT --to 10.20.1.20:80
 sudo iptables -A FORWARD -p tcp -d 10.20.1.20 --dport 80 -j ACCEPT
+
+# Save iptables to survive reboot
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+sudo apt-get -y install iptables-persistent
+sudo bash -c 'iptables-save > /etc/iptables/rules.v4'
 
 # Set webservers in the hosts file
 echo "10.7.5.20       web1" | sudo tee -a /etc/hosts
