9fdfcb4e4c08be869b42458a0dc18af8cd133d9b
Peter <40786627+PeterGriekspoor@users.noreply.github.com>
2021-08-31 16:24:00 +0300

    Update userdata-natgateway.sh

--- userdata-natgateway.sh
+++ userdata-natgateway.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo apt-get install net-tools -y
-sudo apt-get install iptables-persistent -y
-sudo bash -c 'echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sudo apt-get update -y
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+sudo apt-get -y install iptables-persistent
+sudo bash -c 'echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf'
 sudo bash -c 'iptables-save > /etc/iptables/rules.v4'
 sudo ifconfig eth1 10.7.1.10/24 up
 sudo route add -net 10.0.0.0/8 gw 10.7.1.1
@@ -23,4 +26,3 @@ sudo iptables -A FORWARD -p tcp -d 10.20.1.20 --dport 80 -j ACCEPT
 echo "10.7.5.20       web1" | sudo tee -a /etc/hosts
 echo "10.10.1.20      spoke1-web1" | sudo tee -a /etc/hosts
 echo "10.20.1.20      spoke2-web1" | sudo tee -a /etc/hosts
-
