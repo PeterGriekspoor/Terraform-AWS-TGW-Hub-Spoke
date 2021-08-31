e64b88c01dd42330acfd574f66331e9017bdc8af
Peter <40786627+PeterGriekspoor@users.noreply.github.com>
2021-08-31 17:08:44 +0300

    Update userdata-natgateway.sh

--- userdata-natgateway.sh
+++ userdata-natgateway.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 sudo apt-get install net-tools -y
-sudo apt-get update -y
-sudo bash -c 'echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf'
-sudo ifconfig eth1 10.7.1.10/24 up
-sudo route add -net 10.0.0.0/8 gw 10.7.1.1
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -A FORWARD -o eth0 -i eth1 -s 10.0.0.0/8 -m conntrack --ctstate NEW -j ACCEPT
-sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -t nat -F POSTROUTING
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+ifconfig eth1 10.7.1.10/24 up
+route add -net 10.0.0.0/8 gw 10.7.1.1
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -A FORWARD -o eth0 -i eth1 -s 10.0.0.0/8 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -F POSTROUTING
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Add Inbout NAT for Web Servers
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j DNAT --to 10.7.5.20:80
-sudo iptables -A FORWARD -p tcp -d 10.7.5.20 --dport 80 -j ACCEPT
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8081 -j DNAT --to 10.10.1.20:80
-sudo iptables -A FORWARD -p tcp -d 10.10.1.20 --dport 80 -j ACCEPT
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8082 -j DNAT --to 10.20.1.20:80
-sudo iptables -A FORWARD -p tcp -d 10.20.1.20 --dport 80 -j ACCEPT
-
-# Save iptables to survive reboot
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-sudo apt-get -y install iptables-persistent
-sudo bash -c 'iptables-save > /etc/iptables/rules.v4'
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j DNAT --to 10.7.5.20:80
+iptables -A FORWARD -p tcp -d 10.7.5.20 --dport 80 -j ACCEPT
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8081 -j DNAT --to 10.10.1.20:80
+iptables -A FORWARD -p tcp -d 10.10.1.20 --dport 80 -j ACCEPT
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 8082 -j DNAT --to 10.20.1.20:80
+iptables -A FORWARD -p tcp -d 10.20.1.20 --dport 80 -j ACCEPT
 
 # Set webservers in the hosts file
 echo "10.7.5.20       web1" | sudo tee -a /etc/hosts
 echo "10.10.1.20      spoke1-web1" | sudo tee -a /etc/hosts
 echo "10.20.1.20      spoke2-web1" | sudo tee -a /etc/hosts
+© 2021 GitHub, Inc.
